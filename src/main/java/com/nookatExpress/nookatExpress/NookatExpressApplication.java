53a2ca31775ed21db5d117c0eba9f43d6d1e5763
IskandarIT <iskandarchik1995@gmail.com>
2019-06-07 19:14:58 +0600

    first commit

---
+++ src/main/java/com/nookatExpress/nookatExpress/NookatExpressApplication.java
@@ -0,0 +1,13 @@
+package com.nookatExpress.nookatExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NookatExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NookatExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nookatExpress/nookatExpress/NookatExpressApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nookatExpress.nookatExpress;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NookatExpressApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
